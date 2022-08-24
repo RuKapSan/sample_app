134dbb49f3f23064eb7ed2688e9ec561309e5735
RuKapSan <mokan.956@gmail.com>
2022-08-24 14:08:23 +0300

    Add account activation

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -44,19 +44,6 @@ class ValidLoginTest < ValidLogin
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
   end
-
-  test "login with remembering" do
-    log_in_as(@user, remember_me: "1")
-    assert_not cookies[:remember_token].blank?
-  end
-
-  test "login without remembering" do
-    # Log in to set the cookie.
-    log_in_as(@user, remember_me: "1")
-    # Log in again and verify that the cookie is deleted.
-    log_in_as(@user, remember_me: "0")
-    assert cookies[:remember_token].blank?
-  end
 end
 
 class Logout < ValidLogin
